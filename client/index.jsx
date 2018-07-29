53fc896c44a37eff422797404f45cbdbccb300e4
Chris W <christopherkwildenradt@gmail.com>
2018-07-29 16:00:28 -0700

    Setup redux framework

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,10 @@ import App from './components/App.jsx';
 import { Provider } from "react-redux";
 import ReactDOM from 'react-dom';
 import React from 'react';
-// import store from "./store/store";
+import store from "./store/index";
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>, 
     document.getElementById('App'))
